1c512c63908af94eb5487c685cd260befcb2641e
Alex Mathews <alexjmathews@gmail.com>
2018-10-25 11:55:51 -0400

    base plugin

---
+++ basePlugin/InventorAssemblyPlugin/PluginServer.cs
@@ -0,0 +1,69 @@
+﻿/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+// Written by Forge Partner Development
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Inventor;
+
+namespace InventorAssemblyPlugin
+{
+    [Guid("1F599719-CF7D-44FA-8C48-5B12C86A6668")]
+    public class PluginServer : ApplicationAddInServer
+    {
+        // Inventor application object.
+        InventorServer m_inventorServer;
+        SampleAutomation m_automation;
+
+        public dynamic Automation
+        {
+            get
+            {
+                return m_automation;
+            }
+        }
+
+        public void Activate(ApplicationAddInSite AddInSiteObject, bool FirstTime)
+        {
+            Trace.TraceInformation("hello");
+            Trace.TraceInformation(": InventorAssemblyPlugin (" + Assembly.GetExecutingAssembly().GetName().Version.ToString(4) + "): initializing... ");
+
+            // Initialize AddIn members.
+            m_inventorServer = AddInSiteObject.InventorServer;
+            m_automation = new SampleAutomation(m_inventorServer);
+        }
+
+        public void Deactivate()
+        {
+            Trace.TraceInformation(": InventorAssemblyPlugin: deactivating... ");
+
+            // Release objects.
+            Marshal.ReleaseComObject(m_inventorServer);
+            m_inventorServer = null;
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+        public void ExecuteCommand(int CommandID)
+        {
+            // obsolete
+        }
+    }
+}
---
+++ basePlugin/InventorAssemblyPlugin/SampleAutomation.cs
@@ -0,0 +1,89 @@
+﻿using Inventor;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorAssemblyPlugin
+{
+    [ComVisible(true)]
+    public class SampleAutomation
+    {
+        InventorServer inventorApplication;
+        public SampleAutomation(InventorServer inventorApp)
+        {
+            inventorApplication = inventorApp;
+        }
+
+        public void Run(Document doc)
+        {
+            LogTrace("Run called with asdf");
+        }
+
+        public void RunWithArguments(Document doc, NameValueMap map)
+        {
+            //LogInputData(doc, map);
+            LogTrace("Run called with");
+        }
+
+
+        #region Logging utilities
+        private static void LogInputData(Document doc, NameValueMap map)
+        {
+            // dump doc name
+            var traceInfo = new StringBuilder("RunWithArguments called with '");
+            traceInfo.Append(doc.DisplayName);
+
+            traceInfo.Append("'. Parameters: ");
+
+            // dump input parameters
+            // values in map are keyed on _1, _2, etc
+            string[] parameterValues = Enumerable
+                                        .Range(1, map.Count)
+                                        .Select(i => (string)map.Value["_" + i])
+                                        .ToArray();
+            string values = string.Join(", ", parameterValues);
+            traceInfo.Append(values);
+            traceInfo.Append(".");
+
+            LogTrace(traceInfo.ToString());
+        }
+
+        /// <summary>
+        /// Log message with 'trace' log level.
+        /// </summary>
+        private static void LogTrace(string format, params object[] args)
+        {
+            Trace.TraceInformation(format, args);
+        }
+
+        /// <summary>
+        /// Log message with 'trace' log level.
+        /// </summary>
+        private static void LogTrace(string message)
+        {
+            Trace.TraceInformation(message);
+        }
+
+        /// <summary>
+        /// Log message with 'error' log level.
+        /// </summary>
+        private static void LogError(string format, params object[] args)
+        {
+            Trace.TraceError(format, args);
+        }
+
+        /// <summary>
+        /// Log message with 'error' log level.
+        /// </summary>
+        private static void LogError(string message)
+        {
+            Trace.TraceError(message);
+        }
+
+        #endregion
+    }
+}
